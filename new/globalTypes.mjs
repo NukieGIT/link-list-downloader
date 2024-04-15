2181748a3d64271d78f9d8ae042cd89fddc8def5
Nukie <92524294+NukieGIT@users.noreply.github.com>
2024-04-15 05:15:55 +0200

    stuff

--- new/globalTypes.mjs
+++ new/globalTypes.mjs
@@ -1,3 +1,5 @@
+// TODO: Migrate to .d.ts files from JSDoc comments
+
 /**
  * @typedef {Object} EventTargetWithoutDispatch
  * @property {function(string, EventListenerOrEventListenerObject | null, boolean | AddEventListenerOptions=): void} addEventListener
--- new/main.mjs
+++ new/main.mjs
@@ -2,28 +2,15 @@ import UrlDownloader from "/new/downloading/urlDownloader.mjs"
 import { ProgressBarComponent } from "/new/components/progressBarComponent.mjs"
 import { convertDataUnit, getDataUnit } from "/new/utils.mjs"
 
-const progressBar = new ProgressBarComponent()
+// progressBar.unitConverter = (/** @type {number} */ value, /** @type {number} */ max) => {
+//     const maxDataUnit = getDataUnit(max)
+//     const convertedMax = convertDataUnit(max, "Bytes", maxDataUnit)
+//     const convertedValue = convertDataUnit(value, "Bytes", maxDataUnit)
 
-progressBar.unitConverter = (/** @type {number} */ value, /** @type {number} */ max) => {
-    const maxDataUnit = getDataUnit(max)
-    const convertedMax = convertDataUnit(max, "Bytes", maxDataUnit)
-    const convertedValue = convertDataUnit(value, "Bytes", maxDataUnit)
+//     return {
+//         value: parseFloat(convertedValue.toFixed(2)),
+//         max: parseFloat(convertedMax.toFixed(2)),
+//         unit: maxDataUnit
+//     }
+// }
 
-    return {
-        value: convertedValue,
-        max: convertedMax,
-        unit: maxDataUnit
-    }
-}
-
-const ud = new UrlDownloader("https://link.testfile.org/250MB")
-
-ud.downloadEvents.addEventListener("downloadprogress", (/** @type {CustomEvent} */ e) => {
-    progressBar.value = e.detail.length
-})
-
-document.body.appendChild(progressBar)
-
-const fs = await ud.fetchFileSize()
-progressBar.max = fs
-// await ud.download()
